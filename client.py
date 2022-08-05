a355fe96c570f5c070fa3c5103d0f4640ceafa69
xtendev <109812637+xtendev@users.noreply.github.com>
2022-08-05 21:58:59 +0300

    Add test client code and test ini file

---
+++ client.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    from xconf import Config
+
+    c = Config("test.ini")
+
+    print(c)
+    assert c.get("name") == "xtendev"
+    assert c.get("project") == "xconf"
+    assert c.get("non_existent_prop") is None
