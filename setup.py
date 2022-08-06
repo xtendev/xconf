4aa45a5a482c73f53b815fa1643a36d0bb83b2b6
xtendev <109812637+xtendev@users.noreply.github.com>
2022-08-06 21:11:11 +0300

    Change structure to distributable package
    Move main code to __init__.py
    Add setup.py
    Update .gitignore: xconf.egg-info/, build/

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='xconf',
+      version='0.1',
+      description='Yet another package for application configuration',
+      url='https://github.com/xtendev/xconf',
+      author='xtendev',
+      license='MIT',
+      packages=['xconf'],
+      zip_safe=False)
